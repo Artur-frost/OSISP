d69a639633e88def5bb76d626e136fbaefe0967f
Artur-frost <artur.kudlasewitsch@gmail.com>
2023-11-09 19:59:31 +0300

    add Lab1,2

---
+++ OSISP_Lab21/OSISP_Lab21/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Collections.Concurrent;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("============Output Args================");
+
+        foreach (string arg in args)
+        {
+            Console.WriteLine(arg);
+        }
+
+        Console.WriteLine("======================================");
+    }
+}
